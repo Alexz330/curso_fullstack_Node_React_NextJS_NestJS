e02e3dfaaaf3b2157676da0f70eb8bffe3a4d9ff
alexz330 <alexislc333@gmail.com>
2025-02-15 12:01:35 -0600

    feat(router) agregar sistema de rutas

--- DEVTREE/backend/src/server.ts
+++ DEVTREE/backend/src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import router from './router';
+
 
 const app = express();
+app.use('/api',router);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    }
-);
+
 
 export default app;
