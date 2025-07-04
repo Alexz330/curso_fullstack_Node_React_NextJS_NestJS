d00da4ac19a8ad556bbebd6116a4f9cac5c7c8a3
alexz330 <alexislc333@gmail.com>
2025-07-04 17:32:35 -0600

    feat(frontend): agregar navegacion entre vistas LoginView y RegisterView

--- DEVTREE/frontend/src/views/LoginView.tsx
+++ DEVTREE/frontend/src/views/LoginView.tsx
@@ -1,7 +1,12 @@
-export default function LoginView(){
-    return(
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+import { Link } from "react-router-dom";
+
+export default function LoginView() {
+  return (
+    <>
+      <div className="">Login View</div>
+      <nav>
+        <Link to="/auth/register">Ya tiene una cuenta? Inicia Sesión</Link>
+      </nav>
+    </>
+  );
 }
--- DEVTREE/frontend/src/views/RegisterView.tsx
+++ DEVTREE/frontend/src/views/RegisterView.tsx
@@ -1,7 +1,12 @@
-export default function RegisterView(){
-    return(
-        <div>
-            <h1>Register</h1>
-        </div>
-    )
+import { Link } from "react-router-dom";
+
+export default function RegisterView() {
+  return (
+    <>
+      <div className="">Register View</div>
+      <nav>
+        <Link to="/auth/login">No tienes una cuenta? Registrate</Link>
+      </nav>
+    </>
+  );
 }
