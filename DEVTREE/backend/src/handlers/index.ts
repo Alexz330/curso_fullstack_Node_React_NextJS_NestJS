ba68c95b22996175b3d2aecc5b5de19efffe8884
alexz330 <alexislc333@gmail.com>
2025-07-04 12:04:03 -0600

    feat(auth): agregar hasheo de contraseña y control de usuario duplicado en registro

--- DEVTREE/backend/src/handlers/index.ts
+++ DEVTREE/backend/src/handlers/index.ts
@@ -1,24 +1,34 @@
-import User from "../models/User";
 import { Request, Response } from "express";
+import { hashPassword } from "../utils/auth";
+
+import User from "../models/User";
 
 export const createAccount = async (req: Request, res: Response) => {
     const { email } = req.body;
 
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-        const error = new Error("El usuario ya está registrado");
-        error.name = "UserExistsError";
-        res.status(409).json({ error: error.message });
-    }
-
-    const user = new User(req.body);
-
     try {
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            const error = new Error("El usuario ya está registrado");
+            error.name = "UserExistsError";
+            res.status(409).json({ error: error.message });
+            return
+        }
+
+        const user = new User(req.body);
+
+        const hash = await hashPassword(user.password as string);
+
+        user.password = hash;
         await user.save();
-        res.status(201).json({ message: "Usuario creado exitosamente" });
+        console.log("Respuesta: usuario creado");
+
+        res.status(201).json({ message: 'User created successfully', user: user.toObject() });
     } catch (error) {
+        console.log("Respuesta: error al crear el usuario");
         res.status(400).json({
-            error: error instanceof Error ? error.message : "An unknown error occurred",
+            error: error instanceof Error ? error.message : "An unknown error occurred"
         });
     }
 };
