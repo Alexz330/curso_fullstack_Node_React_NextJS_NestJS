a639af13c1c12a0f1a574ccccbbbe582aec57064
alexz330 <alexislc333@gmail.com>
2025-07-04 11:11:51 -0600

    fix(auth): evitar creación de cuenta si el usuario ya está registrado

--- DEVTREE/backend/src/handlers/index.ts
+++ DEVTREE/backend/src/handlers/index.ts
@@ -1,12 +1,24 @@
 import User from "../models/User";
 import { Request, Response } from "express";
 
-export const createAccount = async (req:Request, res:Response) => {
+export const createAccount = async (req: Request, res: Response) => {
+    const { email } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        const error = new Error("El usuario ya está registrado");
+        error.name = "UserExistsError";
+        res.status(409).json({ error: error.message });
+    }
+
     const user = new User(req.body);
+
     try {
         await user.save();
-        res.status(201).json({ message: "User created successfully" });
+        res.status(201).json({ message: "Usuario creado exitosamente" });
     } catch (error) {
-        res.status(400).json({ error: error instanceof Error ? error.message : "An unknown error occurred" });
+        res.status(400).json({
+            error: error instanceof Error ? error.message : "An unknown error occurred",
+        });
     }
 };
