babdbce8f960c2076ab11abb1b83b3cca0bafb54
alexz330 <alexislc333@gmail.com>
2025-07-04 12:38:52 -0600

    feat(auth): implementar validación de datos en registro usando express-validator

--- DEVTREE/backend/src/handlers/index.ts
+++ DEVTREE/backend/src/handlers/index.ts
@@ -1,45 +1,54 @@
 import { Request, Response } from "express";
 import slug from "slug";
+import { validationResult } from "express-validator";
 import { hashPassword } from "../utils/auth";
 import User from "../models/User";
 
 export const createAccount = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
+  // Manjear errores
+  let errors = validationResult(req);
 
-    try {
-        const userExists = await User.findOne({ email });
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+    return;
+  }
 
-        if (userExists) {
-            const error = new Error("El usuario ya está registrado");
-            error.name = "UserExistsError";
-            res.status(409).json({ error: error.message });
-            return;
-        }
+  const { email } = req.body;
 
-        const handle = slug(req.body.handle, '');
-        const handleExists = await User.findOne({ handle });
+  try {
+    const userExists = await User.findOne({ email });
 
-        if (handleExists) {
-            const error = new Error("Nombre de usuario no disponible");
-            error.name = "UserExistsError";
-            res.status(409).json({ error: error.message });
-            return;
-        }
-
-        const user = new User(req.body);
-
-        const hash = await hashPassword(user.password as string);
-
-        user.password = hash;
-        user.handle = handle;
-
-
-        await user.save();
-
-        res.status(201).json({ message: 'Usuario creado exitosamente' });
-    } catch (error) {
-        res.status(400).json({
-            error: error instanceof Error ? error.message : "Un error inesperado ocurrió"
-        });
+    if (userExists) {
+      const error = new Error("El usuario ya está registrado");
+      error.name = "UserExistsError";
+      res.status(409).json({ error: error.message });
+      return;
     }
+
+    const handle = slug(req.body.handle, "");
+    const handleExists = await User.findOne({ handle });
+
+    if (handleExists) {
+      const error = new Error("Nombre de usuario no disponible");
+      error.name = "UserExistsError";
+      res.status(409).json({ error: error.message });
+      return;
+    }
+
+    const user = new User(req.body);
+
+    const hash = await hashPassword(user.password as string);
+
+    user.password = hash;
+    user.handle = handle;
+
+    await user.save();
+
+    res.status(201).json({ message: "Usuario creado exitosamente" });
+  } catch (error) {
+    res.status(400).json({
+      error:
+        error instanceof Error ? error.message : "Un error inesperado ocurrió",
+    });
+  }
 };
--- DEVTREE/backend/src/router.ts
+++ DEVTREE/backend/src/router.ts
@@ -1,9 +1,23 @@
 import { Router } from "express";
 import { createAccount } from "./handlers";
+import { check } from "express-validator";
 const router = Router();
 /** Autenticacion y registro  */
 
-router.post("/auth/register", createAccount);
-
+router.post(
+  "/auth/register",
+  check("email")
+    .isEmail()
+    .withMessage("Por favor ingresa un correo electrónico válido"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("Por favor ingresa una contraseña con al menos 6 caracteres"),
+  check("handle")
+    .isLength({ min: 3 })
+    .withMessage(
+      "Por favor ingresa un nombre de usuario con al menos 3 caracteres"
+    ),
+  createAccount
+);
 
 export default router;
