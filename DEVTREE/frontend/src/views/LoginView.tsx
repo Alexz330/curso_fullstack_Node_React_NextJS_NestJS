c4bbe25b2b5eb8f949d8314d5ef9442023b38618
alexz330 <alexislc333@gmail.com>
2025-07-05 00:44:45 -0600

    feat(frontend): agregar enlace a registro en LoginView

--- DEVTREE/frontend/src/views/LoginView.tsx
+++ DEVTREE/frontend/src/views/LoginView.tsx
@@ -3,8 +3,15 @@ import { Link } from "react-router-dom";
 export default function LoginView() {
   return (
     <>
-      <nav>
-        <Link to="/auth/register">No tienes una cuenta? Registrate</Link>
+      <h1 className="text-4xl text-white font-bold">Iniciar Sesión</h1>
+      
+      <nav className="mt-10">
+        <Link
+          className="text-center text-white text-lg block"
+          to="/auth/register"
+        >
+          No tienes una cuenta? Registrate
+        </Link>
       </nav>
     </>
   );
