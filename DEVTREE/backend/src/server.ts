34662213958290aa5ef431b1ab157502876d30bf
alexz330 <alexislc333@gmail.com>
2025-07-05 09:46:10 -0600

    chore(backend): agregar configuración personalizada de CORS para ambiente local

--- DEVTREE/backend/src/server.ts
+++ DEVTREE/backend/src/server.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+
 import 'dotenv/config'
 
 import router from './router';
 import { connectDB } from './config/db';
-
-const app = express();
+import { corsConfig } from './config/cors';
 
 connectDB();
 
+const app = express();
+
+// Cors
+app.use(cors(corsConfig));
 app.use(express.json());
 app.use('/api',router);
 
