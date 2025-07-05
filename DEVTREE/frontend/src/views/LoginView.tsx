7c5e08bcbd01898d3a519ceb6e2cf0dc8b2be341
alexz330 <alexislc333@gmail.com>
2025-07-04 18:44:40 -0600

    feat(frontend): agregar layout reutilizable para LoginView y RegisterView

--- DEVTREE/frontend/src/views/LoginView.tsx
+++ DEVTREE/frontend/src/views/LoginView.tsx
@@ -4,9 +4,9 @@ import AuthLayout from "../layouts/AuthLayout";
 export default function LoginView() {
   return (
     <AuthLayout>
-    <nav>
-      <Link to="/auth/register">Ya tienes una cuenta? Inicia Sesión</Link>
-    </nav>
-  </AuthLayout>
+      <nav>
+        <Link to="/auth/register">Ya tienes una cuenta? Inicia Sesión</Link>
+      </nav>
+    </AuthLayout>
   );
 }
--- DEVTREE/frontend/src/views/RegisterView.tsx
+++ DEVTREE/frontend/src/views/RegisterView.tsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
+import AuthLayout from "../layouts/AuthLayout";
 
 export default function RegisterView() {
   return (
-    <>
-      <div className="">Register View</div>
+    <AuthLayout>
       <nav>
         <Link to="/auth/login">No tienes una cuenta? Registrate</Link>
       </nav>
-    </>
+    </AuthLayout>
   );
 }
