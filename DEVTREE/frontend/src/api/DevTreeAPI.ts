25fad5e2fc90790820a5039c8d1b0a2a5575630c
alexz330 <alexislc333@gmail.com>
2025-07-06 09:52:25 -0600

    feat(frontend): agregar función getUser para obtener usuario autenticado vía API

---
+++ DEVTREE/frontend/src/api/DevTreeAPI.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import api from "../config/axios";
+
+export async function getUser() {
+  try {
+    const token = localStorage.getItem("AUTH_TOKEN");
+    const { data } = await api.get(`/api/user`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      console.error(error.response?.data.error);
+    }
+  }
+}
