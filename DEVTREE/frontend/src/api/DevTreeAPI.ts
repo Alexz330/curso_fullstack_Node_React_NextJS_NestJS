bdb03ce6440c50257e7775178a09c946690ad84f
alexz330 <alexislc333@gmail.com>
2025-07-06 11:08:23 -0600

    refactor(frontend): tipar la respuesta de getUser usando modelo User para mayor seguridad

--- DEVTREE/frontend/src/api/DevTreeAPI.ts
+++ DEVTREE/frontend/src/api/DevTreeAPI.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 import api from "../config/axios";
+import type { User } from "../types";
 
 export async function getUser() {
   try {
-    const { data } = await api.get(`/api/user`);
+    const { data } = await api.get<User>(`/api/user`);
     return data;
   } catch (error) {
     if (axios.isAxiosError(error) && error.response) {
