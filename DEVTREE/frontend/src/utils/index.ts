074a817f8636040ce1407bbb02fbc825fd7eb262
alexz330 <alexislc333@gmail.com>
2025-07-08 20:36:36 -0600

    feat(utils): agregar función validateUrl para validar URLs en utilidades

--- DEVTREE/frontend/src/utils/index.ts
+++ DEVTREE/frontend/src/utils/index.ts
@@ -1,3 +1,14 @@
 export function classNames(...classes : string[]) {
     return classes.filter(Boolean).join(' ')
 }
+
+
+export function validateUrl(url: string):boolean {
+    try {
+       const pattern = new URL(url);
+       return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
