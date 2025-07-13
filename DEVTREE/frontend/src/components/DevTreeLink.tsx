38c99f78215a06f5fc8718fee5a1dcecff0ee53a
alexz330 <alexislc333@gmail.com>
2025-07-12 20:35:03 -0600

    feat(DevTreeLink): integrar useSortable con id para habilitar drag and drop

--- DEVTREE/frontend/src/components/DevTreeLink.tsx
+++ DEVTREE/frontend/src/components/DevTreeLink.tsx
@@ -1,4 +1,5 @@
-
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 import type { SocialNetwork } from "../types";
 
@@ -7,10 +8,30 @@ type DevTreeLinkProps = {
 };
 
 export default function DevTreeLink({ link }: DevTreeLinkProps) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({
+      id: link.id,
+    });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
   return (
-    <li className="bg-white px-5 py-2 flex items-center gap-5 rounded-lg">
-      <div className="w-12 h-12 bg-cover" style={{backgroundImage: `url('/social/icon_${link.name}.svg')`}}></div>
-      <p className="capitalize">Visita mi: <span className="font-black">{link.name}</span></p>
+    <li
+      style={style}
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      className="bg-white px-5 py-2 flex items-center gap-5 rounded-lg"
+    >
+      <div
+        className="w-12 h-12 bg-cover"
+        style={{ backgroundImage: `url('/social/icon_${link.name}.svg')` }}
+      ></div>
+      <p className="capitalize">
+        Visita mi: <span className="font-black">{link.name}</span>
+      </p>
     </li>
   );
 }
