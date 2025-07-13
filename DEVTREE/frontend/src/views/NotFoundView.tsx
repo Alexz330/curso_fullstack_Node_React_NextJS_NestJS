017eeb565afb2eeec9ec3ed83f71560ef757db06
alexz330 <alexislc333@gmail.com>
2025-07-13 11:58:13 -0600

    feat(NotFoundView): crear vista para página 404

---
+++ DEVTREE/frontend/src/views/NotFoundView.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundView() {
+    return (
+        <div className="flex flex-col items-center  h-screen">
+            <h1 className="text-4xl text-white font-bold">404</h1>
+            <h2 className="text-2xl text-white font-bold">Page not found</h2>
+        </div>
+    );
+}
