c6beffa836f6c17fb80077b9df746a548b44c2f5
alexz330 <alexislc333@gmail.com>
2025-07-12 18:56:59 -0600

    feat(linktree): gestionar alta, baja y reordenamiento de enlaces sociales con id consecutivo

--- DEVTREE/frontend/src/views/LinkTreeView.tsx
+++ DEVTREE/frontend/src/views/LinkTreeView.tsx
@@ -5,7 +5,7 @@ import { validateUrl } from "../utils";
 import { social } from "../data/social";
 import DevTreeInput from "../components/DevTreeInput";
 import { updateProfile } from "../api/DevTreeAPI";
-import type { DevTreeLink, User } from "../types";
+import type { DevTreeLink, SocialNetwork, User } from "../types";
 
 export default function LinkTreeView() {
   const queryClient = useQueryClient();
@@ -24,24 +24,22 @@ export default function LinkTreeView() {
 
   useEffect(() => {
     const updateData = devTreeLinks.map((item) => {
-   
-      const userLink = JSON.parse(user.links).find((link: DevTreeLink) => link.name === item.name);
+      const userLink = JSON.parse(user.links).find(
+        (link: DevTreeLink) => link.name === item.name
+      );
 
       if (userLink) {
-       return {
-         ...item,
-         url: userLink.url,
-         enabled: userLink.enabled,
-
-       };
+        return {
+          ...item,
+          url: userLink.url,
+          enabled: userLink.enabled,
+        };
       }
       return item;
     });
 
     setDevTreeLinks(updateData);
-    
   }, []);
-
 
   const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const url = e.target.value;
@@ -60,6 +58,8 @@ export default function LinkTreeView() {
     setDevTreeLinks(updatedLinks);
   };
 
+  const links: SocialNetwork[] = JSON.parse(user.links);
+
   const handleEnableLink = (socialNetwork: string) => {
     const updatedLinks = devTreeLinks.map((link) => {
       if (link.name === socialNetwork) {
@@ -74,11 +74,57 @@ export default function LinkTreeView() {
       }
       return link;
     });
-    
+
     setDevTreeLinks(updatedLinks);
+    let updateItems: SocialNetwork[] = [];
+    const selectedSocialNetwork = updatedLinks.find(
+      (link) => link.name === socialNetwork
+    );
+
+    if (selectedSocialNetwork?.enabled) {
+      const id = links.filter((link) => link.id).length + 1;
+      if (links.some((link) => link.name === selectedSocialNetwork?.name)) {
+        updateItems = links.map((link) => {
+          if (link.name === selectedSocialNetwork?.name) {
+            return {
+              ...link,
+              id,
+            };
+          }
+          return link;
+        });
+      } else {
+        const newItem = {
+          ...selectedSocialNetwork,
+          id,
+        };
+        updateItems = [...links, newItem];
+      }
+    } else {
+      const indexToUpdate = links.findIndex(
+        (link) => link.name === selectedSocialNetwork?.name
+      );
+      updateItems = links.map((link, index) => {
+        if (link.name === selectedSocialNetwork?.name) {
+          return {
+            ...link,
+            enabled: false,
+            id: 0,
+          };
+        } else if (link.id > indexToUpdate) {
+          return {
+            ...link,
+            id: link.id - 1,
+          };
+        } else {
+          return link;
+        }
+      });
+    }
+
     queryClient.setQueryData(["user"], {
       ...user,
-      links: JSON.stringify(updatedLinks),
+      links: JSON.stringify(updateItems),
     });
   };
 
