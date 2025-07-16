1ccd6e00f31dfc4f413128c947bc310116e85f30
alexz330 <alexislc333@gmail.com>
2025-07-16 14:05:53 -0600

    feat(HomeView): aplicar fondo personalizado con clases bg-home y home-xl

--- DEVTREE/frontend/src/views/HomeView.tsx
+++ DEVTREE/frontend/src/views/HomeView.tsx
@@ -4,6 +4,18 @@ export default function HomeView() {
     return (
         <>
         <Header />
+        <main className="bg-gray-100 py-10 min-h-screen bg-no-repeat bg-right-top bg-home home-xl">
+            <div className="max-w-5xl mx-auto mt-10">
+                <div className="lg:w-1/2 px-10 lg:p-0 space-y-6">
+                    <h1 className="text-6xl font-black">
+                        Todas tus <span className="text-blue-500">redes sociales</span> en un enlace
+                    </h1>
+                    <p className="text-xl text-gray-600">
+                        Únite  a más de 200mil developers compartiendo sus redes sociales, comparte tu perfil de TikTok, Instagram, X, GitHub y más.
+                    </p>
+                </div>
+            </div>
+        </main>
         </>
     );
 }
