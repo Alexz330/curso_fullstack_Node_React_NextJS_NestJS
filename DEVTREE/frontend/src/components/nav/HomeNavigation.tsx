3d4225c99cad7ddc8cd1c4d356669be71ee3cce9
alexz330 <alexislc333@gmail.com>
2025-07-16 12:53:44 -0600

    feat(HomeNavigation): crear navegación principal con enlace a inicio

---
+++ DEVTREE/frontend/src/components/nav/HomeNavigation.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+
+export default function HomeNavigation() {
+
+    return (
+       <>
+            <Link className="text-white p-2 uppercase font-black text-xs cursor-pointer"
+            to="/"
+            >
+                Iniciar Sesión
+            </Link>
+       </>
+    );
+}
